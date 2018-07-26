4527c62a285b35e5aa2896b73cd5fa241700e898
Siri-Dandu <sdandu99@gmail.com>
2018-07-26 18:44:39 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,28 +6,10 @@ class Dog #start of my new object
 		@breed = breed
 	end
 	
-  #These getters make it easier to call upon the code and also make the code easier to read. They can be thought of as changing from a random thing floating around the computer to a method.
 	def name
 		return @name
 	end
 	
-	def age
-		return @age
-	end
-
-  def breed
-    return @breed
-  end 
-
-
-  def age = (new_age)
-    @age = new_age
-  end
-
-
-end
-
-
-dog1 = Dog.new("fido", "6", "Pomeranian")
-dog2 = Dog.new("snoopy", "6", "Pomeranian")
-dog3 = Dog.new("lassie", "6", "Pomerian")
+dog1 = Dog.new("fido")
+dog2 = Dog.new("snoopy")
+dog3 = Dog.new("lassie")
