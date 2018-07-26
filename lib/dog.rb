7705813624c047fd4566280852678667e9155eab
Siri-Dandu <sdandu99@gmail.com>
2018-07-26 18:52:32 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,23 +1,8 @@
 # dog.rb
-class Dog #start of my new object
- 
-def initialize dog("fido", "snoopy", "lassie")
-  @fido = fido
-    @snoopy = snoopy
-    @lassie = lassie
-  end 
-  
-	def fido
-		return @fido
-	end
-	
-	def snoopy
-	  return @snoopy
-	end 
-	
-	def lassie
-	  return @lassie
-	end 
-	
-	end 
-	
+class Dog
+  def initialize(fido, snoopy, lassie)
+    fido = @fido
+      snoopy = @snoopy
+      lassie = @lassie
+    end
+  end
